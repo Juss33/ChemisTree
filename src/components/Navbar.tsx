b61fa867aa728e06f646cdf03388f674c21183c5
nusima-59487 <wongktoliver@gmail.com>
2026-01-16 14:27:03 -0800

    rewrite navbar with bootstrap
    feel free to veto me

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,47 +1,47 @@
 import { FaPlus,FaUser} from "react-icons/fa";
 import {SignInButton,UserButton,useUser} from "@clerk/clerk-react";
 import {Authenticated,Unauthenticated} from "convex/react"
-import {Link,useNavigate} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 import CreateDropdown from "./CreateDropDown";
 import {useState} from "react"
 import SearchBar from "./SearchBar";
-import "../styles/Navbar.css";
+import { Button, Container, Navbar } from "react-bootstrap";
 
 const NavBar = () => {
   const [showDropdown,setShowDropdown] = useState(false);
   const {user} = useUser();
   const navigate = useNavigate();
 
-  return <nav className="navbar">
-    <div className="navbar-content">
-      <Link to="/" className="logo-link">
-        <div className="logo-container">
-          <img src="/LOGOCHEMSITREE.png" alt="logo" width="40" height="40"></img>
-          <span className="site-name">ChemisTree</span>
-        </div>
-      </Link>
+  return <Navbar className="bg-body-tertiary py-0">
+    <Container>
+      <Navbar.Brand href="/">
+        <img src="/LOGOCHEMSITREE.png" alt="logo" width="40" height="40"></img>
+        <span className="d-none d-md-inline">ChemisTree</span>
+      </Navbar.Brand>
       <SearchBar/>
-      <div className="nav-actions">
-        <Unauthenticated>
-          <SignInButton mode="modal">
-            <button className="sign-in-button">Sign In</button>
-          </SignInButton>
-        </Unauthenticated>
-        <Authenticated>
-          <div className="dropdown-container">
-            <button className="icon-buttons" onClick={() => setShowDropdown(true)}>
-              <FaPlus />
-            </button>
-            {showDropdown && (<CreateDropdown isOpen={showDropdown} onClose={()=> setShowDropdown(false)} />)}
-          </div>
-            <button className="icon-button" onClick={() => user?.username && navigate(`/u/${user.username}`)} title="View Profile">
-              <FaUser />
-            </button>
-            <UserButton />
-        </Authenticated>
-      </div>
-    </div>
-  </nav>;
+      <Unauthenticated>
+        <SignInButton mode="modal">
+          <Button variant="warning">Sign In</Button>
+        </SignInButton>
+      </Unauthenticated>
+      <Authenticated>
+        <div className="dropdown-container position-relative">
+          <Button variant="light" onClick={() => setShowDropdown(true)}>
+            <FaPlus />
+          </Button>
+          {showDropdown && (<CreateDropdown isOpen={showDropdown} onClose={()=> setShowDropdown(false)} />)}
+        </div>
+        <Button variant="light" onClick={() => user?.username && navigate(`/u/${user.username}`)} title="View Profile" className="me-2">
+          <FaUser />
+        </Button>
+        <UserButton />
+      </Authenticated>
+    </Container>
+  </Navbar>; 
+
+  // sign in button colours: 
+  // #FF4500
+  // hover: #ff5722
 }
 
 export default NavBar;
