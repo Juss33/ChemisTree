2da38b869f52453d9253d080fb9aadb31ce09cbf
nusima-59487 <wongktoliver@gmail.com>
2026-02-08 12:39:40 -0800

    removed hard-coded keys
    for safety and also better development
    @Jus33 add them in your env vars!

--- src/main.tsx
+++ src/main.tsx
@@ -9,10 +9,15 @@ import { ConvexReactClient } from "convex/react";
 console.log("Convex URL:", import.meta.env.VITE_CONVEX_URL);
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
+const clerkPublishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY; 
+// @Juss33 again when i commit add this to .env
+if (!clerkPublishableKey) {
+  throw new Error("env var VITE_CLERK_PUBLISHABLE_KEY is not set");
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey="pk_test_aHVtYmxlLWtpd2ktNzQuY2xlcmsuYWNjb3VudHMuZGV2JA">
+    <ClerkProvider publishableKey={clerkPublishableKey}>
       <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
         <App />
       </ConvexProviderWithClerk>
