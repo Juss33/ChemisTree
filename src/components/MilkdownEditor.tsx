148d1554138ed803b1a2cebfb827cff7fa64cfd1
nusima-59487 <wongktoliver@gmail.com>
2026-02-12 23:17:12 -0800

    refactor milkdown editor + add link, list support
    styles need change

---
+++ src/components/MilkdownEditor.tsx
@@ -0,0 +1,70 @@
+import { defaultValueCtx, Editor, editorViewOptionsCtx, rootCtx } from "@milkdown/kit/core";
+import { commonmark, linkSchema, } from "@milkdown/kit/preset/commonmark";
+import { InputRule, } from "@milkdown/kit/prose/inputrules";
+import { $inputRule } from "@milkdown/kit/utils";
+import { Milkdown, useEditor } from "@milkdown/react";
+import { listItemBlockComponent } from "@milkdown/components/list-item-block"
+import { configureLinkTooltip, linkTooltipPlugin } from "@milkdown/kit/component/link-tooltip";
+
+// input rules
+/**
+ * replaces text with a link on typing the markdown format
+ * 
+ * \[text\]\(url\)
+ * 
+ */
+const AddLinkInputRule = $inputRule((ctx) => {
+    return new InputRule(/\[(.*?)\]\((.*?)\)$/, (state, match, start, end) => {
+        const [okay, text, url] = match; 
+        const { tr } = state; 
+        if (okay) {
+            tr.replaceWith(start, end, state.schema.text(text + " "))
+                .addMark(
+                    start, 
+                    start + text.length, 
+                    linkSchema.type(ctx).create({ href: url })
+                )
+        }
+        return tr; 
+    })
+})
+
+/** Options in creating new editor */
+interface MilkdownEditorOptions {
+    /** Default content to show on load */ 
+    defaultMarkdown: string, 
+    /** whether the editor is editable (readonly or not?) */
+    canEdit: boolean, 
+}
+
+/** element for the milkdown editor
+ * 
+ * - editing posts / comments
+ * - viewing posts (`MilkdownEditorOptions.canEdit` = false)
+ */
+export const MilkdownEditor: React.FC<MilkdownEditorOptions> = ({
+    defaultMarkdown = "", 
+    canEdit = false
+}: MilkdownEditorOptions) => {
+    const { get } = useEditor((root) =>
+        Editor.make()
+        // .config(nord)
+        .config((ctx) => {
+            ctx.set(rootCtx, root);
+            ctx.set(defaultValueCtx, defaultMarkdown);
+            ctx.update(editorViewOptionsCtx, (prev) => ({
+                ...prev,
+                editable: () => canEdit,
+            }));
+            
+        })
+        .config(configureLinkTooltip)
+        .use(commonmark)
+        .use(listItemBlockComponent)
+        .use(linkTooltipPlugin)
+        .use(AddLinkInputRule)
+        ,  
+    );
+
+    return <Milkdown />;
+};
--- src/pages/PostCreatePage.tsx
+++ src/pages/PostCreatePage.tsx
@@ -1,7 +1,6 @@
 import { Container } from "react-bootstrap";
-import { defaultValueCtx, Editor, rootCtx } from "@milkdown/kit/core";
-import { commonmark } from "@milkdown/kit/preset/commonmark";
 import { Milkdown, MilkdownProvider, useEditor } from "@milkdown/react";
+import { MilkdownEditor } from "../components/MilkdownEditor";
 
 const defaultMarkdown = 
 `# This is a sample text
@@ -14,28 +13,22 @@ Chemistree is the goat
 
 
 Feel free to type here...
+
+Click on [this link](https://bongo.cat/) to get a cute cat!
+
+- sample list
+- this will be fixed
+- I HATE TROUBLESHOOTING
 `
 
 
-const MilkdownEditor: React.FC = () => {
-    const { get } = useEditor((root) =>
-        Editor.make()
-        // .config(nord)
-        .config((ctx) => {
-            ctx.set(rootCtx, root);
-            ctx.set(defaultValueCtx, defaultMarkdown); 
-        })
-        .use(commonmark),
-    );
 
-    return <Milkdown />;
-};
 const PostCreatePage = () => {
 
     return (
         <Container>
             <MilkdownProvider>
-                <MilkdownEditor />
+                <MilkdownEditor defaultMarkdown={defaultMarkdown} canEdit={true} />
             </MilkdownProvider>
         </Container>
     )
