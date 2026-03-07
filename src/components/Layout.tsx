08d4383ce399b0b4495caf8de3bfe5fdc3158fbc
Jus33 <justin26young@gmail.com>
2026-03-07 14:08:06 -0800

    Update Layout.tsx

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,25 +4,24 @@ import { Col, Row } from "react-bootstrap";
 import SideBar from "./Sidebar";
 import { useState } from "react";
 
-function Layout({ children }: any) {
+function Layout() {
   const [open, setOpen] = useState(true);
+
   return (
     <div className="app-container">
-        <NavBar /> 
-        <Row className="g-0 flex-grow-1">
+      <NavBar />
+      <button onClick={() => setOpen(!open)}>Toggle</button>
+
+      <Row className="g-0 flex-grow-1">
+        {open && (
           <Col xs={3} lg={2} className="text-bg-sidebar d-none d-md-block">
-            <div>
-              <button onClick={() => setOpen(!open)}>Toggle</button>
-
-              <SideBar open={open} />
-
-              <main>{children}</main>
-            </div>
+            <SideBar open={open} />
           </Col>
-          <Col>
-            <Outlet />
-          </Col>
-        </Row>
+        )}
+        <Col>
+          <Outlet />
+        </Col>
+      </Row>
     </div>
   );
 }
