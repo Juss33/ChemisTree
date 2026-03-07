2c58872337d70e7f36c3246f91639728eb334bc2
Jus33 <justin26young@gmail.com>
2026-03-07 13:40:24 -0800

    SideBar Toggle attempt

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,14 +2,22 @@ import {Outlet} from "react-router-dom";
 import NavBar from "./Navbar";
 import { Col, Row } from "react-bootstrap";
 import SideBar from "./Sidebar";
+import { useState } from "react";
 
-const Layout = () => {
+function Layout({ children }: any) {
+  const [open, setOpen] = useState(true);
   return (
     <div className="app-container">
         <NavBar /> 
         <Row className="g-0 flex-grow-1">
           <Col xs={3} lg={2} className="text-bg-sidebar d-none d-md-block">
-            <SideBar />
+            <div>
+              <button onClick={() => setOpen(!open)}>Toggle</button>
+
+              <SideBar open={open} />
+
+              <main>{children}</main>
+            </div>
           </Col>
           <Col>
             <Outlet />
