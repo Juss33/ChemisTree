967dc5b792f8db92d2e0fb7fb7e7943d8d2b9735
Jus33 <justin26young@gmail.com>
2026-04-23 16:22:35 -0700

    Update acidbase.ts

--- src/Quizzes/acidbase.ts
+++ src/Quizzes/acidbase.ts
@@ -1,10 +1,14 @@
 const acidbaseQuiz = {
     title: "Acid and Base Quiz",
-    questions: [{
+    questions: [
+        {
         question: "Which are the following is an acid?",
         options: ["H20","HCl","KOH","NaOH"],
-        answer: 1
-
-    }]
+        answer: 1},
+        {
+        question: "Which are the following is an acid?",
+        options: ["H20","HCl","KOH","NaOH"],
+        answer: 1}
+    ]
 }
 export default acidbaseQuiz
